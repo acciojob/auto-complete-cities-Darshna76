1ebc2d3bf8392450aa70e06f12777d14ebc25045
Darshna <darshnakhatri989@gmail.com>
2023-06-18 14:43:27 +0530

    submitting code-Sun Jun 18 2023 14:43:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./../styles/App.css";
+import Autocomplete from "./Autocomplete";
 
-const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
-
-const Autocomplete = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-
-  useEffect(() => {
-    const filteredSuggestions = fruits.filter(
-      (fruit) =>
-        fruit.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
-    );
-    setSuggestions(filteredSuggestions);
-  }, [inputValue]);
-
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
+const App = ()=>{
   return (
-     <div className="container">
-      
-      
-      <div className="autocomplete">
-      <div className="title">
-        <h1>Search item</h1>
-        
-
-      </div>
-      <br />
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Search for a fruit"
-      />
-      <br />
-      <ul>
-        {suggestions.map((suggestion) => (
-          <li key={suggestion}>{suggestion}</li>
-        ))}
-      </ul>
+    <div>
+      <Autocomplete />
     </div>
+  )
+}
 
-     </div>
-    
-  );
-};
-
-export default Autocomplete;
+export default App;
---
+++ src/components/Autocomplete.js
@@ -0,0 +1,50 @@
+import React,{useState,useEffect} from "react";
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+const Autocomplete = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const filteredSuggestions = fruits.filter(
+      (fruit) =>
+        fruit.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
+    );
+    setSuggestions(filteredSuggestions);
+  }, [inputValue]);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+     <div className="container">
+      
+      
+      <div className="autocomplete">
+      <div className="title">
+        <h1>Search item</h1>
+        
+
+      </div>
+      <br />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Search for a fruit"
+      />
+      <br />
+      <ul>
+        {suggestions.map((suggestion) => (
+          <li key={suggestion}>{suggestion}</li>
+        ))}
+      </ul>
+    </div>
+
+     </div>
+    
+  );
+};
+
+export default Autocomplete;
